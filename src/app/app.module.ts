74a5ccef7a4e2d375babb112e6d81b0bc6d738df
Sean Hung <yunshanghong@gmail.com>
2022-01-05 22:53:31 +0800

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+	{ path: '', component: AppComponent },
+	{ path: '**', redirectTo: "/" },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		RouterModule.forRoot(routes, { initialNavigation: 'enabled' }),
+		BrowserModule.withServerTransition({ appId: 'serverApp' }),
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
