712254477221b8fd65559796e80d2f245ac3f702
Sean Hung <yunshanghong@gmail.com>
2022-01-19 14:05:30 +0800

    header modify

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -12,6 +12,7 @@ import { UserService } from 'src/app/services/user.service';
 export class HeaderComponent implements OnInit{
 
 	user: User = null;
+	showSearch: boolean = false;
 
 	constructor(
         private router: Router,
