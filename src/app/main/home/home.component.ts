982fcd16fbba9bc0595a4ed45fb990588cc06a86
Sean Hung <yunshanghong@gmail.com>
2022-01-25 14:51:04 +0800

    modify home create btns

--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -14,6 +14,7 @@ import { isPlatformBrowser } from '@angular/common';
 })
 export class HomeComponent extends BaseComponent implements OnInit{
 
+    createBtns: Array<string> = ["New PC", "New CV", "Used PC", "Used CV"]; 
     searchTabs: Array<string> = ["Approved", "Pending Approval", "Declined", "Draft", "Canceled", "Pending myInfo"]
     isAnimated: boolean = false;
     orderItems: Array<OrderItem> = [];
